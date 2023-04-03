6b2774f0f99a15d3786b0799943b774cdf8f55c5
Maxsuel Trajano <maxsuelwtrajano@gmail.com>
2023-04-03 13:36:12 +0100

    Post route working

--- routes/message.js
+++ routes/message.js
@@ -4,21 +4,24 @@ const router = express.Router()
 
 
 router.get('/new', (req, res) =>{
-    // res.send('new message')
-    // creating a new empty instance of the Message Model
-    console.log('Empty Model:', new Message())
     res.render('messages/newMessage', {message: new Message()})
 })
 
-// async operations
+
 router.post('/', async (req, res) =>{
-    console.log("Post route working")
-    // const message = await new Message({
-    //     title: req.body.title,
-    //     author: req.body.author,
-    //     message: req.body.message
-    // })
-    // console.log(message)
+    let message = new Message({
+        title: req.body.title,
+        author: req.body.author,
+        message: req.body.message
+    })
+    try{
+        message = await message.save()
+        res.redirect('/')
+    }
+    catch(e){
+        res.render('messages/newMessage', {message: message})
+    }
+    
 })
 
 module.exports = router
